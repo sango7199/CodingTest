170156c2aedd77bc188ab5ad38fbb848cc044f21
san <sango7199@gmail.com>
2024-04-16 14:34:29 +0900

    4월 16일 (화) - 큐

---
+++ src/Que/_0416_14729.java
@@ -0,0 +1,13 @@
+package Que;
+
+import java.util.Scanner;
+
+public class _0416_14729 {
+    // 칠무해
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+    }
+}
