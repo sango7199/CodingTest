d76d56fc299789d7c36e618beea102c8d3c30c07
san <sango7199@gmail.com>
2024-04-17 14:06:39 +0900

    4월 17일 (수) - 큐

--- src/Que/_0416_14729.java
+++ src/Que/_0416_14729.java
@@ -1,5 +1,7 @@
 package Que;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class _0416_14729 {
@@ -9,5 +11,29 @@ public class _0416_14729 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
+        // 우선순위 큐
+        PriorityQueue<Float> priQue = new PriorityQueue<>(7, Comparator.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            float score = sc.nextFloat();
+            if (priQue.size() < 7) {
+                priQue.offer(score);
+            } else {
+                if (score < priQue.peek()) {
+                    priQue.poll();
+                    priQue.offer(score);
+                }
+            }
+        }
+        sc.close();
+
+        // 정렬 후 출력
+        PriorityQueue<Float> sortedPriQue = new PriorityQueue<>();
+        while (!priQue.isEmpty()) {
+            sortedPriQue.add(priQue.poll());
+        }
+
+        while (!sortedPriQue.isEmpty()) {
+            System.out.printf("%.3f\n", sortedPriQue.poll());
+        }
     }
 }
