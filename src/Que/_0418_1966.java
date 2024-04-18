84ef76d081dde7a003a2327c34fa37bd0eebb585
san <sango7199@gmail.com>
2024-04-18 14:31:22 +0900

    4월 18일 (목) - 큐

---
+++ src/Que/_0418_1966.java
@@ -0,0 +1,21 @@
+package Que;
+
+import java.util.Scanner;
+
+public class _0418_1966 {
+    // 프린터 큐
+    public static void main(String[] args) {
+        // 입력 및 선언
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int N; int M; int Pri; // Priority
+
+        for (int i = 0; i < T; i++) {
+            N = sc.nextInt();
+            M = sc.nextInt();
+            Pri = sc.nextInt();
+
+
+        }
+    }
+}
