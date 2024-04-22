db20ab3ff082a7fc0f042ac961d6a25bfdcdf0ff
san <sango7199@gmail.com>
2024-04-23 01:57:17 +0900

    4월 22일 (월) - 큐 (BR ver 시간초과 실패)

--- src/Que/_0422_28107.java
+++ src/Que/_0422_28107.java
@@ -1,27 +1,33 @@
 package Que;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class _0422_28107 {
-    // 회전초밥
-    public static void main(String[] args) {
+    // 회전초밥 (입력 BR ver)
+    public static void main(String[] args) throws IOException {
         // 입력 및 선언
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 손님의 수
-        int M = sc.nextInt(); // 초밥의 수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 손님의 수
+        int M = Integer.parseInt(st.nextToken()); // 초밥의 수
         int[][] customers = new int[N][];
         for (int i = 0; i < N; i++) {
-            int k = sc.nextInt(); // 손님의 주문목록 갯수
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());; // 손님의 주문목록 갯수
             customers[i] = new int[k]; // 해당 손님의 주문 목록 배열을 크기 k로 초기화
             for (int j = 0; j < k; j++) {
-                customers[i][j] = sc.nextInt();
+                customers[i][j] = Integer.parseInt(st.nextToken());;
             }
         }
         Queue<Integer> sushi = new LinkedList<>(); // 회전 초밥
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            sushi.add(sc.nextInt());
+            sushi.add(Integer.parseInt(st.nextToken()));
         }
         int[] sushiEaten = new int[N]; // 먹힌 초밥 (출력값)
 
