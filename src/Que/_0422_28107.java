1db1e87c6affdb7ec1ca189c4c23b2ecf7c0a5cd
san <sango7199@gmail.com>
2024-04-29 23:55:56 +0900

    4월 29일 (월) - 큐(HashMap ver)

--- src/Que/_0422_28107.java
+++ src/Que/_0422_28107.java
@@ -13,17 +13,18 @@ public class _0422_28107 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()); // 손님의 수
         int M = Integer.parseInt(st.nextToken()); // 초밥의 수
-        HashMap<Integer, ArrayList<Integer>> customers = new HashMap<>(); // K 손님, V 손님의 초밥 목록
+        HashMap<Integer, ArrayList<Integer>> sushiPreferences = new HashMap<>(); // K 초밥 종류, V 손님 번호
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int k = Integer.parseInt(st.nextToken());; // 손님의 주문목록 갯수
             // 해당 손님의 주문 목록 배열을 크기 k로 초기화
             for (int j = 0; j < k; j++) {
                 int A = Integer.parseInt(st.nextToken());
-                customers.putIfAbsent(A, new ArrayList<>());
-                // 여기까지 일단
+                sushiPreferences.putIfAbsent(A, new ArrayList<>());
+                sushiPreferences.get(A).add(i);
             }
         }
+
         Queue<Integer> sushi = new LinkedList<>(); // 회전 초밥
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
@@ -34,19 +35,19 @@ public class _0422_28107 {
         // 먹힌 초밥 계산
         while(!sushi.isEmpty()) {
             int curSushi = sushi.poll(); // 현재 스시
-            loop : for (int i = 0; i < N; i++) {
-                for (int j = 0; j < customers[i].length; j++) {
-                    if (customers[i][j] == curSushi) {
-                        sushiEaten[i]++;
-                        break loop;
-                    }
+            if(sushiPreferences.containsKey(curSushi)) {
+                for (int customer : sushiPreferences.get(curSushi)) {
+                    sushiEaten[customer]++;
+                    break;
                 }
             }
         }
 
         // 출력
-        for (int output: sushiEaten) {
-            System.out.print(output + " ");
+        StringBuilder sb = new StringBuilder();
+        for (int count : sushiEaten) {
+            sb.append(count).append(" ");
         }
+        System.out.print(sb.toString());
     }
 }
