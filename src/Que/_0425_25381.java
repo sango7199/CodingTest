ef1b9cc592abd56e3f6b74a3a909a679321dddc0
san <sango7199@gmail.com>
2024-05-28 13:19:53 +0900

    5월 28일 (화) - 큐

--- src/Que/_0425_25381.java
+++
@@ -1,8 +0,0 @@
-package Que;
-
-public class _0425_25381 {
-    // ABBC
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Que/_0528_19644.java
@@ -0,0 +1,19 @@
+package Que;
+
+import java.util.Scanner;
+
+public class _0528_19644 {
+    // 좀비 떼가 기관총 진지에도 오다니
+    public static void main(String[] args) {
+        // 입력 및 선언
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt(); // 진지 앞쪽 거리
+        int ML = sc.nextInt(); // 기관총 사거리
+        int MK = sc.nextInt(); // 기관총 살상력
+        int C = sc.nextInt(); // 수평 지뢰 살상력
+
+        for (int i = 0; i < L; i++) { // 좀비의 체력
+
+        }
+    }
+}
